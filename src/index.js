f531151e5c57acb52d51da2777b9109c6e017ec5
Eina Onting <eonting@y5creative.com>
2019-04-17 16:13:18 -0700

    add sanitize

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import '../node_modules/sanitize.css/sanitize.css'; 
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
